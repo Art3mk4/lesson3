5e05684564c41ff2f7810491198eda47a28bf844
art3mk4 <art3mk4@gmail.com>
2023-01-10 14:58:23 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,56 @@
+import * as React from "react"
+import { produce } from "immer";
+import Grid from "./components/Grid"
+
+
+const windowHeight = window.innerHeight
+const windowWidth = window.innerWidth
+const resolution = 33
+const numRows = Math.floor(windowHeight / resolution)
+const numCols = Math.floor(windowWidth / resolution)
+
+
+const onCell = (event, i, j) => {
+  if (event.buttons === 1)
+  // Primary button pressed
+  // Toggle cell status
+  setGrid(
+    produce(grid, (gridCopy) => {
+      gridCopy[i][j] = grid[i][j] ? 0 : 1
+    })
+  )
+}
+
+const getEmptyGrid = () => {
+  return Array.from({ length: numRows }).map(() =>
+    Array.from({ length: numCols }).fill(0)
+  );
+};
+
+
+
+/*export const App: React.FC<{}> = () => {
+    return (<div data-testid="app-test-id">Hello React!</div>)
+}*/
+
+export const App: React.FC<{}> = () => {
+  const [grid, setGrid] = React.useState(getEmptyGrid);
+  const onCell = (event, i, j) => {
+    if (event.buttons === 1)
+      // Primary button pressed
+      // Toggle cell status
+      setGrid(
+        produce(grid, (gridCopy) => {
+          gridCopy[i][j] = grid[i][j] ? 0 : 1;
+        })
+      );
+  };
+
+  return (
+    <div data-testid="app-test-id">
+        <Grid numCols={numCols} grid={grid} onCell={onCell} />
+    </div>
+  );
+}
+
+export default App;
