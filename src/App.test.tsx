8a3b9ed7e0ec19adb90313e18b06fa064c4ed894
art3mk4 <art3mk4@gmail.com>
2023-01-10 15:56:19 +0600

    add eslint and husky

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,40 +1,33 @@
-import React from 'react'
-import { render, screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
-import { App } from './App'
-import { Grid } from './components/Grid'
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { App } from './App';
+import { Grid } from './components/Grid';
 
-/*describe("App", () => {
-    it("renders the heading", () => {
-        const result = shallow(<App />).contains(<div>Hello React!</div>);
-        expect(result).toBeTruthy();
-    });
-});*/
+describe('App component', () => {
+	it('should renders app component', () => {
+		render(<App />);
+		const app = screen.getAllByTestId('app-test-id');
+		expect(app).toHaveLength(1);
+		const grid = screen.getAllByTestId('grid-test-id');
+		expect(grid).toHaveLength(1);
+	});
+});
 
-describe("App component", () => {
-    it("should renders app component", () => {
-        render(<App />)
-        const app = screen.getAllByTestId('app-test-id')
-        expect(app).toHaveLength(1)
-        const grid = screen.getAllByTestId('grid-test-id')
-        expect(grid).toHaveLength(1)
-    })
-})
+describe('Grid component', () => {
+	it('should change grid component', async () => {
+		const { container } = render(<App />);
+		const grid = screen.getAllByTestId('grid-test-id');
+		expect(grid).toHaveLength(1);
 
-describe("Grid component", () => {
-    it("should change grid component", async () => {
-        const {container} = render(<App />)
-        const grid = screen.getAllByTestId('grid-test-id')
-        expect(grid).toHaveLength(1)
+		const divElement0 = screen.getAllByTestId('grid-test-cell-0-0');
+		await userEvent.click(divElement0[0]);
+		const divClicked0 = container.getElementsByClassName('grid-test-cell-0-0');
+		expect(divClicked0.length).toBe(1);
 
-        const divElement0 = screen.getAllByTestId('grid-test-cell-0-0')
-        await userEvent.click(divElement0[0])
-        const divClicked0 = container.getElementsByClassName('grid-test-cell-0-0')
-        expect(divClicked0.length).toBe(1);
-
-        const divElement5 = screen.getAllByTestId('grid-test-cell-5-5')
-        await userEvent.click(divElement5[0])
-        const divClicked5 = container.getElementsByClassName('grid-test-cell-5-5')
-        expect(divClicked5.length).toBe(1);
-    })
-})
+		const divElement5 = screen.getAllByTestId('grid-test-cell-5-5');
+		await userEvent.click(divElement5[0]);
+		const divClicked5 = container.getElementsByClassName('grid-test-cell-5-5');
+		expect(divClicked5.length).toBe(1);
+	});
+});
--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,58 +1,47 @@
-import React from "react";
-import "./Grid.css";
+import React from 'react';
+import './Grid.css';
 
 const cellSize = 30; // In pixels
-const cellColors = [
-  "ffffff",
-  "7ed957",
-  "38b6ff",
-  "5271ff",
-  "8c52ff",
-  "cb6ce6",
-  "ff66c4",
-  "ff914d",
-  "ff5757",
-  "000000",
-]; // In hexadecimal
+const cellColors = ['ffffff', '7ed957']; // In hexadecimal
 
 function Grid(props) {
-  const getCellColor = (cell) => {
-    if (cell > 9) cell = 9;
-    return `#${cellColors[cell]}`
-  };
+	const getCellColor = (cell) => {
+		if (cell > 9) cell = 9;
+		return `#${cellColors[cell]}`;
+	};
 
-  const handleClick: React.MouseEventHandler<HTMLDivElement> = (e) => {
-    event.target.classList.add(event.target.getAttribute('data-testid'))
-    e.preventDefault()
-  }
+	const handleClick: React.MouseEventHandler<HTMLDivElement> = (e) => {
+		event.target.classList.add(event.target.getAttribute('data-testid'));
+		e.preventDefault();
+	};
 
-  return (
-    <div
-      className="grid"
-      data-testid="grid-test-id"
-      style={{
-        gridTemplateColumns: `repeat(${props.numCols}, ${cellSize}px)`,
-      }}
-    >
-      {props.grid.map((row, i) =>
-        row.map((cell, j) => (
-          <div
-            data-testid={`grid-test-cell-${i}-${j}`}
-            key={`${i}-${j}`}
-            onMouseDown={(event) => props.onCell(event, i, j)}
-            onMouseOver={(event) => props.onCell(event, i, j)}
-            onClick={handleClick}
-            style={{
-              width: cellSize,
-              height: cellSize,
-              // Set color based on cell iteration
-              backgroundColor: getCellColor(cell),
-            }}
-          />
-        ))
-      )}
-    </div>
-  );
+	return (
+		<div
+			className="grid"
+			data-testid="grid-test-id"
+			style={{
+				gridTemplateColumns: `repeat(${props.numCols}, ${cellSize}px)`,
+			}}
+		>
+			{props.grid.map((row, i) =>
+				row.map((cell, j) => (
+					<div
+						data-testid={`grid-test-cell-${i}-${j}`}
+						key={`${i}-${j}`}
+						onMouseDown={(event) => props.onCell(event, i, j)}
+						onMouseOver={(event) => props.onCell(event, i, j)}
+						onClick={handleClick}
+						style={{
+							width: cellSize,
+							height: cellSize,
+							// Set color based on cell iteration
+							backgroundColor: getCellColor(cell),
+						}}
+					/>
+				))
+			)}
+		</div>
+	);
 }
 
 export default Grid;
