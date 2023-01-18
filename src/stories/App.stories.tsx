4b1f80b73a210158dbd0ede3c98f396edf62a091
art3mk4 <art3mk4@gmail.com>
2023-01-18 09:06:57 +0600

    add app component template export

--- src/stories/App.stories.tsx
+++ src/stories/App.stories.tsx
@@ -11,4 +11,6 @@ export default {
     }
 } as ComponentMeta<typeof App>
 
-const Template: ComponentStory<typeof App> = (args) => <App {...args} />
+export const Template: ComponentStory<typeof App> = (args) => {
+    return <App {...args} />
+}
